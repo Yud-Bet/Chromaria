ea685f56a5e70be325b6d32be08999108c04aeda
Yud-Bet <66458858+Yud-Bet@users.noreply.github.com>
2021-08-09 12:58:51 +0700

    Add spdlog and premake

--- Chromaria/src/Chromaria/EntryPoint.h
+++ Chromaria/src/Chromaria/EntryPoint.h
@@ -6,6 +6,10 @@ extern Chromaria::Application* Chromaria::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Chromaria::Log::Init();
+	CM_CORE_WARN("Hello");
+	CM_FATAL("Bonjour {0}", 3);
+
 	auto app = Chromaria::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Chromaria/src/Chromaria/Log.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <memory>
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Chromaria {
+
+	class CHROMARIA_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+#define CM_CORE_ERROR(...)	Chromaria::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define CM_CORE_WARN(...)	Chromaria::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define CM_CORE_INFO(...)	Chromaria::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define CM_CORE_TRACE(...)	Chromaria::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define CM_CORE_FATAL(...)	Chromaria::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+#define CM_ERROR(...)		Chromaria::Log::GetClientLogger()->error(__VA_ARGS__)
+#define CM_WARN(...)		Chromaria::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define CM_INFO(...)		Chromaria::Log::GetClientLogger()->info(__VA_ARGS__)
+#define CM_TRACE(...)		Chromaria::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define CM_FATAL(...)		Chromaria::Log::GetClientLogger()->critical(__VA_ARGS__)
