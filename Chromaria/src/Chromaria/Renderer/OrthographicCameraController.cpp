c2eeb7a1599386c36dc5c7a416cc6cf9cf7dd3e1
Yud-Bet <66458858+Yud-Bet@users.noreply.github.com>
2021-10-03 22:00:51 +0700

    Fix camera movement when rolled

--- Chromaria/src/Chromaria/Renderer/OrthographicCameraController.cpp
+++ Chromaria/src/Chromaria/Renderer/OrthographicCameraController.cpp
@@ -15,20 +15,24 @@ namespace Chromaria {
 	{
 		if (Input::IsKeyPressed(CM_KEY_A))
 		{
-			m_CameraPosition.x -= m_CameraTranslationSpeed * ts;
+			m_CameraPosition.x -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+			m_CameraPosition.y -= sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 		}
 		else if (Input::IsKeyPressed(CM_KEY_D))
 		{
-			m_CameraPosition.x += m_CameraTranslationSpeed * ts;
+			m_CameraPosition.x += cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+			m_CameraPosition.y += sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 		}
 
 		if (Input::IsKeyPressed(CM_KEY_S))
 		{
-			m_CameraPosition.y -= m_CameraTranslationSpeed * ts;
+			m_CameraPosition.x += -sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+			m_CameraPosition.y += cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 		}
 		else if (Input::IsKeyPressed(CM_KEY_W))
 		{
-			m_CameraPosition.y += m_CameraTranslationSpeed * ts;
+			m_CameraPosition.x -= -sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+			m_CameraPosition.y -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 		}
 
 		if (m_Rotation)
@@ -41,6 +45,11 @@ namespace Chromaria {
 			{
 				m_CameraRotation += m_CameraRotationSpeed * ts;
 			}
+
+			if (m_CameraRotation > 180.0f)
+				m_CameraRotation -= 360.0f;
+			else if (m_CameraRotation <= -180.0f)
+				m_CameraRotation += 360.0f;
 
 			m_Camera.SetRotation(m_CameraRotation);
 		}
--- Chromaria/src/Chromaria/Renderer/OrthographicCameraController.h
+++ Chromaria/src/Chromaria/Renderer/OrthographicCameraController.h
@@ -33,7 +33,7 @@ namespace Chromaria {
 
 		glm::vec3 m_CameraPosition = { 0.0f, 0.0f, 0.0f };
 		float m_CameraRotation = 0;
-		float m_CameraTranslationSpeed = 1.0f, m_CameraRotationSpeed = 5.0f;
+		float m_CameraTranslationSpeed = 1.0f, m_CameraRotationSpeed = 10.0f;
 	};
 
 }
