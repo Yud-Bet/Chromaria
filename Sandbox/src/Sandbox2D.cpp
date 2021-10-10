97b7b3e242879cb761e164cee0e42af1af19e3d5
Yud-Bet <66458858+Yud-Bet@users.noreply.github.com>
2021-10-10 17:31:31 +0700

    Demo profiling

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -4,6 +4,45 @@
 
 #include <glm/gtc/type_ptr.hpp>
 
+#include <chrono>
+
+template<typename Fn>
+class Timer
+{
+public:
+	Timer(const char* name, Fn&& func)
+		:m_Name(name), m_Stopped(false), m_Func(func)
+	{
+		m_StartTimePoint = std::chrono::high_resolution_clock::now();
+	}
+
+	~Timer()
+	{
+		if (!m_Stopped)
+			Stop();
+	}
+
+	void Stop()
+	{
+		auto endTimePoint = std::chrono::high_resolution_clock::now();
+
+		long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimePoint).time_since_epoch().count();
+		long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimePoint).time_since_epoch().count();
+
+		m_Stopped = true;
+
+		auto duration = (end - start) * 0.001f;
+		m_Func({ m_Name, duration });
+	}
+private:
+	const char* m_Name;
+	Fn m_Func;
+	std::chrono::time_point<std::chrono::steady_clock> m_StartTimePoint;
+	bool m_Stopped;
+};
+
+#define PROFILE_SCOPE(name) Timer timer##__LINE__(name, [&](ProfileResult profileResult) { m_ProfileResults.push_back(profileResult); })
+
 Sandbox2D::Sandbox2D()
 	:Layer("Sandbox2D"), m_CameraController(16.0f / 9.0f, true)
 {
@@ -20,16 +59,27 @@ void Sandbox2D::OnDetach()
 
 void Sandbox2D::OnUpdate(Chromaria::Timestep ts)
 {
-	m_CameraController.OnUpdate(ts);
+	PROFILE_SCOPE("Sandbox2D::OnUpdate");
 
-	Chromaria::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
-	Chromaria::RenderCommand::Clear();
+	{
+		PROFILE_SCOPE("CameraController::OnUpdate");
+		m_CameraController.OnUpdate(ts);
+	}
 
-	Chromaria::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	Chromaria::Renderer2D::DrawQuad({ 1.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-	Chromaria::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.5f, 1.0f }, { 0.2f, 0.3f, 1.0f, 1.0f });
-	Chromaria::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, {0.8f, 0.3f, 0.2f, 1.0f});
-	Chromaria::Renderer2D::EndScene();
+	{
+		PROFILE_SCOPE("Render preparation");
+		Chromaria::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+		Chromaria::RenderCommand::Clear();
+	}
+
+	{
+		PROFILE_SCOPE("Draw call");
+		Chromaria::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		Chromaria::Renderer2D::DrawQuad({ 1.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Chromaria::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.5f, 1.0f }, { 0.2f, 0.3f, 1.0f, 1.0f });
+		Chromaria::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, { 0.15f, 0.15f, 0.15f, 1.0f });
+		Chromaria::Renderer2D::EndScene();
+	}
 
 	/*m_FlatColorShader->Bind();
 	std::dynamic_pointer_cast<Chromaria::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);*/
@@ -38,7 +88,16 @@ void Sandbox2D::OnUpdate(Chromaria::Timestep ts)
 void Sandbox2D::OnImGuiRender()
 {
 	ImGui::Begin("Setting");
-	ImGui::ColorPicker4("Square color", glm::value_ptr(m_SquareColor));
+	ImGui::ColorEdit4("Square color", glm::value_ptr(m_SquareColor));
+
+	for (auto& result : m_ProfileResults)
+	{
+		char label[50];
+		strcpy(label, " %.3fms ");
+		strcat(label, result.Name);
+		ImGui::Text(label, result.Time);
+	}
+	m_ProfileResults.clear();
 	ImGui::End();
 }
 
--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -22,5 +22,13 @@ private:
 
 	Chromaria::OrthographicCameraController m_CameraController;
 
+	struct ProfileResult
+	{
+		const char* Name;
+		float Time;
+	};
+
+	std::vector<ProfileResult> m_ProfileResults;
+
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 };
